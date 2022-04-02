52db5d2e56e6bef6879bf269c9fb038a99aca62e
dell <binodsubedi125@gmail.com>
2022-04-02 12:37:09 +0545

    v1 final-push

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useRef } from 'react';
 import axios from 'axios';
 import SiteBackend from '../apis/SiteBackend';
+import './../styles/dashboard.css';
 
 const Dashboard = () => {
   const imagee = useRef();
+  const title = useRef();
+  const bodyText = useRef();
 
   useEffect(() => {
     // const change = true;
@@ -29,24 +32,40 @@ const Dashboard = () => {
     res();
   }, []);
 
-  const send = (e) => {
+  const send = async (e) => {
     e.preventDefault();
     const form = new FormData();
 
-    form.append('title', 'Insta');
-    form.append('bodyText', 'It is a test');
+    form.append('title', title.current.value);
+    form.append('bodyText', bodyText.current.value);
     form.append('photo', imagee.current.files[0]);
 
-    console.log(imagee.current.files[0]);
+    // console.log(imagee.current.files[0]);
 
     // axios({
     //   method: 'POST',
     //   url: 'http://192.168.1.154:3000/api/v1/post/createPost',
     //   form,
     // });
-    SiteBackend.post('/api/v1/post/createPost', form, {
+
+    const resp = await SiteBackend.post('/api/v1/post/createPost', form, {
       withCredentials: true,
     });
+
+    console.log(resp);
+
+    if (resp.data.type === 'Success') {
+      alert('Request successful');
+      // title.current.value = '';
+      // bodyText.current.value = '';
+      // imagee.current.files[0] = null;
+      // } else {
+      //   alert('Request un-successful');
+    }
+
+    // if (!resp) {
+    //   alert('Un-successful');
+    // }
   };
 
   const logout = async (e) => {
@@ -67,6 +86,14 @@ const Dashboard = () => {
       </button>
       <div className="Dashboard__options">
         <input type="file" className="photto" ref={imagee} />
+        <div className="Dashboard__options--title">
+          <label htmlFor="title">Title:</label>
+          <input type="text" id="title" ref={title} />
+        </div>
+        <div className="Dashboard__options--body">
+          <textarea type="text" id="body" cols={60} rows={20} ref={bodyText} />
+        </div>
+
         <button className="submit" onClick={(e) => send(e)}>
           Submit
         </button>
